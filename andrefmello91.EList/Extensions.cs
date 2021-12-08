14c2c3d7fb2330c588f8000527d919230e653c0a
Andre <andremello91@gmail.com>
2021-12-08 15:31:11 -0400

    Adopted file-scoped namespaces

--- andrefmello91.EList/Extensions.cs
+++ andrefmello91.EList/Extensions.cs
@@ -3,30 +3,29 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-namespace andrefmello91.EList
+namespace andrefmello91.EList;
+
+/// <summary>
+///     <see cref="EList{T}" /> extensions.
+/// </summary>
+public static class Extensions
 {
+
+	#region Methods
+
 	/// <summary>
-	///     <see cref="EList{T}" /> extensions.
+	///     Creates an <see cref="EList{T}" /> based in an <see cref="IEnumerable{T}" />.
 	/// </summary>
-	public static class Extensions
-	{
+	/// <inheritdoc cref="IEList{T}" />
+	/// <param name="collection">The collection to transform.</param>
+	public static EList<T>? ToEList<T>(this IEnumerable<T?>? collection)
+		where T : IEquatable<T>, IComparable<T> =>
+		collection is null
+			? null
+			: !collection.Any()
+				? new EList<T>()
+				: new EList<T>(collection);
 
-		#region Methods
+	#endregion
 
-		/// <summary>
-		///     Creates an <see cref="EList{T}" /> based in an <see cref="IEnumerable{T}" />.
-		/// </summary>
-		/// <inheritdoc cref="IEList{T}" />
-		/// <param name="collection">The collection to transform.</param>
-		public static EList<T>? ToEList<T>(this IEnumerable<T?>? collection)
-			where T : IEquatable<T>, IComparable<T> =>
-			collection is null
-				? null
-				: !collection.Any()
-					? new EList<T>()
-					: new EList<T>(collection);
-
-		#endregion
-
-	}
 }
